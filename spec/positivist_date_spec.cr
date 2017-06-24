7e19f76306b94b4355adccbd1438e6ff8404e390
TPei <t.s.peikert@gmail.com>
2017-06-24 17:53:58 +0200

    initial setup

---
+++ spec/positivist_date_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe PositivistDate do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/positivist_date.cr
@@ -0,0 +1,5 @@
+require "./positivist_date/*"
+
+module PositivistDate
+  # TODO Put your code here
+end
